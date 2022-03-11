108a26a3015f6af7da1ec1d5088755dde7445fc3
apal6981 <ashton.palacios1@gmail.com>
2022-03-11 12:57:37 -0700

    fixed topdown view

--- topdown_hsv.py
+++ topdown_hsv.py
@@ -1,12 +1,12 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-vidcap = cv2.VideoCapture('Monster Truck Sample Code/Video200.avi')
+vidcap = cv.VideoCapture('Video200.avi')
 success,frame = vidcap.read()
 count = 0
 
-camera_points_orig = np.array([[240,130],[640-240, 150],[0, 250], [640, 250]])
+camera_points_orig = np.array([[150,150],[640-150, 150],[50, 250], [640-50, 250]])
 camera_points = camera_points_orig.copy()
 camera_points = camera_points.astype(np.float32)
 # birds_eye_points = np.array([[0,0],[500,0],[0,500],[500,500]],dtype=np.float32)
@@ -29,24 +29,24 @@ while success:
     height = 250
     dim = (width, height)
 
-    frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+    frame = cv.resize(frame, dim, interpolation = cv.INTER_AREA)
 
-    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # cv2.imshow("HSI", hsvImage)
-    h, hsv_s, v = cv2.split(hsvImage)
+    hsvImage = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    # cv.imshow("HSI", hsvImage)
+    h, hsv_s, v = cv.split(hsvImage)
     # hsv_split = np.concatenate((h,s,v),axis=1)
-    ret,thresh1 = cv2.threshold(hsv_s,75,255,cv2.THRESH_BINARY)
+    ret,thresh1 = cv.threshold(hsv_s,75,255,cv.THRESH_BINARY)
 
     kernel = np.ones((5,5),np.uint8)
-    saturation = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN, kernel)
+    saturation = cv.morphologyEx(thresh1, cv.MORPH_OPEN, kernel)
 
     lines_b = np.bitwise_and(saturation, frame[:,:,0]) # Blue
     lines_g = np.bitwise_and(saturation, frame[:,:,1]) # Green
     lines = np.bitwise_or(lines_b, lines_g)
     obstacles = np.bitwise_and(saturation, frame[:,:,2]) # Red
 
-    ret,thresh_lines = cv2.threshold(lines,175,255,cv2.THRESH_BINARY)
-    ret,thresh_obstacles = cv2.threshold(obstacles,175,255,cv2.THRESH_BINARY)
+    ret,thresh_lines = cv.threshold(lines,175,255,cv.THRESH_BINARY)
+    ret,thresh_obstacles = cv.threshold(obstacles,175,255,cv.THRESH_BINARY)
 
     # Chop off top of cones
     
@@ -55,23 +55,23 @@ while success:
             if thresh_obstacles[i,j] == 255 and thresh_obstacles[i-20, j] == 255:
                 thresh_obstacles[0:i-20, j:j+75] = 0
                 j += 75
-    # cv2.imshow("Obstacles", thresh_obstacles)
+    # cv.imshow("Obstacles", thresh_obstacles)
             
     combined = np.bitwise_or(thresh_lines, thresh_obstacles)
 
     concat = np.concatenate((thresh_lines, thresh_obstacles), axis=1)
-    # cv2.imshow("Split HSV", concat)
+    # cv.imshow("Split HSV", concat)
 
-    # gray = cv2.cvtColor(combined,cv2.COLOR_BGR2GRAY)
+    # gray = cv.cvtColor(combined,cv.COLOR_BGR2GRAY)
     for point in camera_points_orig:
-        cv2.circle(combined, point,4,(0,255,0),cv2.FILLED)
-        # cv2.circle(frame,point,4,(0,255,0),cv2.FILLED)
-    M = cv2.getPerspectiveTransform(camera_points, dst)
-    warped_img = cv2.warpPerspective(combined, M, (IMAGE_W, IMAGE_H))
-    # warped_img = cv2.warpPerspective(frame, M, (IMAGE_W, IMAGE_H))
+        cv.circle(combined, point,4,(0,255,0),cv.FILLED)
+        cv.circle(frame,point,4,(0,255,0),cv.FILLED)
+    M = cv.getPerspectiveTransform(camera_points, dst)
+    warped_img = cv.warpPerspective(combined, M, (IMAGE_W, IMAGE_H))
+    # warped_img = cv.warpPerspective(frame, M, (IMAGE_W, IMAGE_H))
     
     threshold = 50
-    res = 10
+    res = 5
     shape = (int(IMAGE_H/res), int(IMAGE_W/res))
     warped_img = warped_img[0:shape[0]*res, 0:shape[1]*res]   
     sh = shape[0],warped_img.shape[0]//shape[0],shape[1],warped_img.shape[1]//shape[1]
@@ -91,11 +91,12 @@ while success:
     #         if val > grid_thresh:
     #             grid[i,j] = 1
 
-    plt.imshow(grid, origin='upper', cmap='binary')
-    # cv2.imshow("grid", grid)
-    # cv2.imshow("RGB", combined)
-    # cv2.imshow("warped",warped_img)
-    # cv2.imshow("frame",frame)
-    plt.show()
-    cv2.waitKey(0)
+    # plt.imshow(grid, origin='upper', cmap='binary')
+    cv.imshow("grid", grid)
+    cv.imshow("RGB", combined)
+    cv.imshow("warped",warped_img)
+
+    cv.imshow("frame",frame)
+    # plt.show()
+    cv.waitKey(0)
     count += 1
