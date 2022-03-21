6bdecb223a5f21c6cfb831c96dc6241fbb7dce3e
Seba <seb.zapata8@gmail.com>
2022-03-21 15:51:16 -0600

    added script to record depth and rgb together

---
+++ depth_video_recorder.py
@@ -0,0 +1,24 @@
+
+from RealSense import *
+import numpy as np
+import cv2 as cv
+from Arduino import Arduino
+
+
+rs = None
+Car = None
+writer = None
+
+try:
+    rs = RealSense("/dev/video2", RS_VGA)		# RS_VGA, RS_720P, or RS_1080P
+    (time, rgb, depth, accel, gyro) = rs.getData()
+    depth_writer = cv.VideoWriter('depth_video_seba.avi', cv.VideoWriter_fourcc(*'MJPG'), 30, (rgb.shape[1], rgb.shape[0]), True)
+    rgb_writer = cv.VideoWriter('rgb_video_seba.avi', cv.VideoWriter_fourcc(*'MJPG'), 30, (rgb.shape[1], rgb.shape[0]), True)
+    while True:
+        (time, rgb, depth, accel, gyro) = rs.getData()
+        depth_writer.write(depth)
+        rgb_writer.write(rgb)
+        cv.waitKey(1)
+
+except Exception as ex:
+    print(f"Exception!: {ex}")
